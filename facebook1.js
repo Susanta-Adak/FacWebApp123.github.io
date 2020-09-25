d6f1b154a9f695fcd7a710e2d6088c9d08527c75
Susanta-Adak <susantaadak513@gmail.com>
2020-09-25 18:50:41 +0530

    first update

--- facebook1.js
+++ facebook1.js
@@ -1,30 +1,97 @@
-function formvalidation(){
-var first_name= document.getElementById("fname").value;
-var first_name_fild= document.getElementById("fname");
-var warning= document.getElementById("warning");
+var first_name = document.getElementById("fname");
+var warning = document.getElementById("warning");
 
+var last_name= document.getElementById("lname");
+var warning1= document.getElementById("warning1");
+
+var user_name= document.getElementById("username");
+var warning2= document.getElementById("warning2");
+
+var password= document.getElementById("password");
+var warning3= document.getElementById("warning3");
+
+var day=document.getElementById("day");
+var month=document.getElementById("month");
+var year=document.getElementById("year");
+
+var male=document.getElementById("male");
+var female=document.getElementById("female");
+var other=document.getElementById("other");
+
+function formvalidation(){
   
-   if(first_name=="")
+
+  //start of first name validaction
+   if(first_name.value == "")
    {
      warning.style.display="inline-block";
-     first_name_fild.style.border="2px solid red";
-     first_name.placeholder.value="bcncb";
-     return false;
+     first_name.style.border="2px solid red";
+     
    }
-   if(first_name.length>0){
+     else{
       warning.style.display="none";
-      first_name_fild.style.border="2px solid black";
-      return false;
+      first_name.style.border="2px solid #2980b9";
+      
+   }
+
+   //start of last name validaction
+   if(last_name.value == "")
+   {
+     warning1.style.display="inline-block";
+     last_name.style.border="2px solid red";
+   }
+   else{
+     warning1.style.display="none";
+      last_name.style.border="2px solid #2980b9";
    }
   
+   //start of  name validaction
+   
+   if(user_name.value=="")
+   {
+     warning2.style.display="inline-block";
+     user_name.style.border="2px solid red";
+   }
+   else{
+      warning2.style.display="none";
+      user_name.style.border="2px solid #2980b9";
+   }
+ //start of  password validaction
+   if(password.value=="")
+   {
+     warning3.style.display="inline-block";
+     password.style.border="2px solid red";
+   }
+   else{
+      warning3.style.display="none";
+      password.style.border="2px solid #2980b9";
+     
+   }
+   // start of day validation
+   if(day.value=="Day")
+   {
+     day.style. border="2px solid red";
+   }
+   else{
+    day.style. border="1px solid white";
+   }
 
-}
-function openfacebook(){
-   window.open("https://www.facebook.com","_self");
-}
-document.getElementById("header_login").addEventListener("click",openfacebook);
-function openyoutube(){
-   window.open("https://youtu.be/Q1R5Y1RGxSs","_self");
-}
-document.getElementById("submit").addEventListener("click",openyoutube);
+    // start of dob validation
+    if(month.value=="Month")
+    {
+      month.style. border="2px solid red";
+    }
+    else{
+      month.style. border="1px solid white";
+    }
 
+    // start of dob validation
+    if(year.value=="Year")
+    {
+      year.style. border="2px solid red";
+    }
+    else{
+      year.style. border="1px solid white";
+      
+    }
+}
